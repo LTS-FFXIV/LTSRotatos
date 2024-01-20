d71f52cfb5403e5500105a40254756bd5806cf96
LTS-FFXIV <127939494+LTS-FFXIV@users.noreply.github.com>
2024-01-20 06:51:52 -0600

    Update AssemblyInfo.cs

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,5 +1,1 @@
-﻿//Put your hash into this one if you want someone say hello to you when they are using RotationSolver in Duty with you.
-[assembly: AuthorHash(Hash = "")]
-
-//Something to show your link.
-[assembly: AssemblyLink(Donate = "", UserName = "LTS", Repository = "LTSRotatos")]
+[assembly: AssemblyLink(Donate = "", UserName = "LTS-FFXIV", Repository = "LTSRotatos")]
